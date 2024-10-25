30f5c13b6770d3a944cd295896298e7cc63f851a
sautaboy <sautaboy.giri00123@gmail.com>
2024-10-25 21:50:51 +0545

    something changed

--- gsap.js
+++ gsap.js
@@ -1,6 +1,7 @@
 const tl = gsap.timeline()
 tl.to("#loading-screen", {
-    opacity: 1
+    opacity: 1,
+    backgroundColor: "#fff"
 }).from("#loading-screen img", {
     opacity: 0,
     duration: 1,
@@ -10,10 +11,68 @@ tl.to("#loading-screen", {
 }).to("#loading-screen", {
     opacity: 0,
     scale: 20,
+    y: "-100%",
     duration: 2,
     backgroundColor: "transparent",
-    ease: "power2.inOut"
+    ease: "power2.inOut",
+    onComplete: () => {
+        gsap.set("#loading-screen", { display: "none" });
+    },
 }).to("header,main,footer", {
     opacity: 1,
     ease: "power2.out"
+})
+
+
+
+
+document.querySelector("#whole").addEventListener("mousemove", (dets) => {
+    gsap.to("#curser", {
+        x: dets.x,
+        y: dets.y,
+        duration: 1,
+        ease: "back.out"
+    })
+})
+
+
+document.querySelectorAll("img").forEach((each) => {
+    each.addEventListener
+        ("mouseenter", (dets) => {
+            gsap.to("#curser", {
+                x: dets.x,
+                y: dets.y,
+                scale: 3,
+                duration: 1,
+                backgroundColor: "red",
+                ease: "back.out"
+            })
+        })
+})
+document.querySelectorAll("img").forEach((each) => {
+    each.addEventListener
+        ("mouseleave", (dets) => {
+            gsap.to("#curser", {
+                x: dets.x,
+                y: dets.y,
+                scale: 1,
+                duration: 1,
+                backgroundColor: "green",
+                ease: "back.out"
+            })
+        })
+})
+
+
+
+gsap.to("#sliding-gallery img", {
+    transform: "translateX(-270%)",
+    scrollTrigger: {
+        trigger: "#sliding-gallery",
+        scroller: "body",
+        start: "top 0%",
+        end: "top -30%",
+        scrub: 2,
+        pin: true,
+    }
 })
